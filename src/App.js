edb885cb9a3d8f2c91b37e87f448bd12a670ff0e
Vince Liem <vince.liem@iodigital.com>
2022-10-14 12:46:33 +0200

    with localstorage

--- src/App.js
+++ src/App.js
@@ -12,20 +12,29 @@ ${intention}
 ## email:`
 
 function App() {
-  const [key, setKey] = useState("");
-  const [myName, setMyName] = useState("");
+  // if "key" in localStorage, use it, else empty string
+  const [key, setKey] = useState(localStorage.getItem("key") || "");
+  const [myName, setMyName] = useState(localStorage.getItem("myName") || "");
   const [intention, setIntention] = useState("");
   const [output, setOutput] = useState("");
+
+  // save apikey in localstorage
+  const saveKey = (e) => {
+    e.preventDefault();
+    localStorage.setItem("key", key);
+  }
+
+  const saveMyName = (e) => {
+    e.preventDefault();
+    localStorage.setItem("myName", myName);
+  }
 
   const submit = async (e) => {
     e.preventDefault();
 
-    console.log(instruction(intention, myName));
-
     const data = await fetch('https://api.openai.com/v1/completions', {
       method: 'POST',
       headers: {
-        // prettier-ignore
         Authorization: `Bearer ${key}`,
         'Content-Type': 'application/json'
       },
@@ -50,12 +59,19 @@ function App() {
     >
 
       <label htmlFor="api-key">Api Key</label>
-      <input id="api-key" type="text" value={key} onChange={(e) => setKey(e.target.value)}/>
+      <div style={{display: "flex", flexWrap: "nowrap"}}>
+        <input id="api-key" type="text" value={key} onChange={(e) => setKey(e.target.value)} style={{width: "100%"}}/>
+        <button type="button" onClick={saveKey}>save</button>
+      </div>
 
       <div style={{height: "2rem"}}/>
 
       <label htmlFor="my-name">My Name</label>
-      <input id="my-name" type="text" value={myName} onChange={(e) => setMyName(e.target.value)}/>
+      <div style={{display: "flex", flexWrap: "nowrap"}}>
+        <input id="my-name" type="text" value={myName} onChange={(e) => setMyName(e.target.value)}
+               style={{width: "100%"}}/>
+        <button type="button" onClick={saveMyName}>save</button>
+      </div>
 
       <div style={{height: "2rem"}}/>
 
